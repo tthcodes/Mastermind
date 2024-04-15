be28ce610a6c509d5788d8381f25032f6739f2e0
tthcodes <huynh2016@gmail.com>
2024-04-15 17:04:36 -0500

    Added react router capability and CSS file

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App';
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+    <App/>
+    </Router>
+  </React.StrictMode> 
+);
