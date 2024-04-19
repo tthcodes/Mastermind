73dc153d52011ca2587eb0775d6a709e11c6caa3
tthcodes <huynh2016@gmail.com>
2024-04-19 02:09:40 -0500

    Begin building out user router

--- src/server/routers/userRouter.js
+++ src/server/routers/userRouter.js
@@ -1,2 +1,12 @@
 // Router for requests related to the user
 import express from 'express';
+
+const userRouter = express.Router();
+
+// Check if user is logged in on home page. 
+// Show user score if they are logged in (Home, Play, GameOver)
+// Display user name on HomePage, and GameOver if logged in
+// Calculate total earned points w/ every win that also updates user score. 
+userRouter.get(
+  '/', 
+);
