bffb255576c91dad84b12bc4ae83b974757dcccd
tthcodes <huynh2016@gmail.com>
2024-04-19 00:32:27 -0500

    Completed model schemas

--- src/server/models/settingsModel.js
+++ src/server/models/settingsModel.js
@@ -3,14 +3,17 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const settingSchema = new Schema({
-  firstName: {type: String, required: false},
-  username: {type: String, required: true},
-  password: {type: String, required: true},
-  created_at: {
-    type: Date,
-    default: Date.now,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User', 
+    unique: true,
+    required: true
   },
-});
+  maxGuessCount: {type: Number, default: 10},
+  minNum: {type: Number, default: 0},
+  maxNum: {type: Number, default: 7},
+  answerSize: {type: Number, default: 4}
+}, { timestamps: true });
 
 const settingsModel =  mongoose.model('Settings', settingSchema);
 export default settingsModel;
--- src/server/models/userModel.js
+++ src/server/models/userModel.js
@@ -1,15 +1,21 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bycrypt'
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
   firstName: {type: String, required: false},
-  username: {type: String, required: true},
+  username: {type: String, required: true, unique: true},
   password: {type: String, required: true},
-  created_at: {
-    type: Date,
-    default: Date.now,
-  },
+  userScore: {type: Number, default: 0}
+}, { timestamps: true });
+
+// Hash user password before saving
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password') || this.isNew) { // Check for when pw modified or not yet saved
+    this.password = await bcrypt.hash(this.password, 12); // Async hash oif pw with salt round of 12
+  }
+  next();
 });
 
 const userModel =  mongoose.model('User', userSchema);
