daeab2ce2f0f8fa50dad9e97d8297828eb37d026
tthcodes <huynh2016@gmail.com>
2024-04-22 04:16:47 -0500

    More configurations to try to get CI/CD to work

--- jest.setup.js
+++ jest.setup.js
@@ -1,13 +1,21 @@
 import mongoose from "mongoose";
-import connectToTestDB from "./src/server/testDatabase";
+// import connectToTestDB from "./src/server/testDatabase";
+import { MongoMemoryServer } from "mongodb-memory-server";
 // File for creating, starting, and connecting to mock mongo server 
+
+let mongoServer;
 
 beforeAll(async() => {
   // console.log('Waiting to connect to Test DB...')
-  await connectToTestDB();
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  process.env.MONGODB_URI = uri;
+  await mongoose.connect(uri, { dbName: 'TestUserDB' })
   // console.log('Connected to Test DB from jest.setup.js!')
 });
 
 afterAll(async() => {
   await mongoose.disconnect();
+  await mongoServer.stop();
 });
+
