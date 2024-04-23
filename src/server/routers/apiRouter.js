bc985e0014c659a2ebecb443b279b6923514b135
tthcodes <huynh2016@gmail.com>
2024-04-23 01:42:00 -0500

    Added client-side input data sanitation and checking for query and body

--- src/server/routers/apiRouter.js
+++ src/server/routers/apiRouter.js
@@ -2,25 +2,28 @@
 import express from 'express';
 import numController from '../controllers/numController.js';
 import authController from '../controllers/authController.js';
-import { query } from 'express-validator' // new, prevents SQL injections and sanitizes
-//eventually import validationErrorHandler for considerations
+import { query } from 'express-validator' // prevents injections and sanitizes incoming req.query data
 import userRouter from './userRouter.js';
+import validationHandler from '../controllers/sanitizeController.js';
 
 const apiRouter = express.Router();
 
-// Number API-specific routes
+// User-specific routes, /api/user
+
+apiRouter.use('/user', userRouter);
+
+// Sanitize sent query data from client request when generating new answer, /api/generate-answer/
 apiRouter.get(
   '/generate-answer/',
-  query('length').isInt({ min: 2, max: 10}),
-  query('min').isInt({ min: 0, max: 9}),
-  query('max').isInt({ min: 1, max: 9}),
+  query('length').isInt({ min: 2, max: 6}), // no setting for under 2 or over 6 nums
+  query('min').isInt({ min: 0, max: 8}), // settings disallow minNums outside this range
+  query('max').isInt({ min: 1, max: 9}), // settings disallow maxNums outside this range
+  validationHandler,
   numController.generateAnswer
 );
 
-// User-specific routes, /api/user
-apiRouter.use('/user', userRouter);
-
-// Session-related routes
+// Session-related routes, no sanitation needed since session data handled by server, 
+  // /api/auth/verify-session
 apiRouter.get('/auth/verify-session', authController.verifySession);
 
 export default apiRouter;
