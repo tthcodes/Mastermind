54f29a0488a3aeeb0dfef29f8a956917cd753f78
tthcodes <huynh2016@gmail.com>
2024-04-23 12:42:02 -0500

    Settings Model Update

--- src/server/models/settingsModel.js
+++ src/server/models/settingsModel.js
@@ -4,15 +4,15 @@ const { Schema } = mongoose;
 
 const settingSchema = new Schema({
   user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User', 
-    unique: true,
+    type: Schema.Types.ObjectId, // specifies user field will store ObjectId to ref obj in 'user' collection
+    ref: 'User', // Tells mongoose the objectId references docs in 'User' collection. 
+    unique: true, // No 2 docs can refer to same user 
     required: true
   },
   maxGuessCount: {type: Number, default: 10},
   minNum: {type: Number, default: 0},
   maxNum: {type: Number, default: 7},
-  answerSize: {type: Number, default: 4}
+  numCount: {type: Number, default: 4}
 }, { timestamps: true });
 
 const settingsModel =  mongoose.model('Settings', settingSchema);
